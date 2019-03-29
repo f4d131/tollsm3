8dc33c112a7781d04b8ed8252904d742112ce865
fad333 <47511104+fad333@users.noreply.github.com>
2019-03-29 16:33:49 +0800

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,65 @@
+import os, sys
+
+print ("\033[1;32mMasukin NAMA&KATASANDI")
+
+username = 'f4dl'      
+
+password = 'bacotlu'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mSukses", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mKata sandi salah njir!!!\033[00m"
+
+			print "Back Login\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mKata sandinya Bacotlu   !!!\033[00m"
+
+		print "Back Login\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+restart()
